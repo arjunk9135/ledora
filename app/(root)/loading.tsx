3653c98d73e780f94471747ee767e430b51db64d
Arjun K <arjunk9135@gmail.com>
2025-04-10 22:45:59 +0530

    Transfers fix and loader

---
+++ app/(root)/loading.tsx
@@ -0,0 +1,9 @@
+// app/(root)/payment-transfer/loading.tsx
+export default function Loading() {
+    return (
+      <div className="flex h-screen items-center justify-center bg-gray-100">
+        <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500" />
+      </div>
+    );
+  }
+  
